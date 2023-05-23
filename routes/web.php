7e5c95f29f425b45b40b3a153d9cd03278a2150f
al <8c.alfan@gmail.com>
2023-05-24 00:50:13 +0700

    Edit route

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
